207b05e758243e86b9c1f42b2c0744309b8e7a50
tonytongqingwu <tony.wu@dexcom.com>
2022-10-18 16:59:20 -0700

    ios added

---
+++ apython/iosappium.py
@@ -0,0 +1,234 @@
+import os
+import sys
+import datetime
+import base64
+import re
+import time
+from time import sleep
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.common.exceptions import NoSuchElementException
+from apython.utils import get_battery_level, get_top_info, remove_appium
+
+
+class AppiumIOS:
+    def __del__(self):
+        try:
+            self.driver.stop_client()
+        except Exception as e:
+            print(e)
+
+    def __init__(self, device_id, port_num='4723', dexcom_app_type='G7'):
+        self.device_id = device_id
+        self.port_num = port_num
+        self.dexcom_app_type = dexcom_app_type
+        # Common desired capibilties and driver creation
+        self.common_desired_cap = {
+            "udid": self.device_id,
+            "automationName": "XCUITest",
+            "wdaLocalPort": self.port_num,
+            "autoAcceptAlerts": True,
+            "xcodeOrgId": 'P762WHM474',
+            "xcodeSigningId": 'iPhone Developer',
+            "newCommandTimeout": 600
+        }
+        try:
+            self.driver = webdriver.Remote("http://localhost:" + self.port_num + "/wd/hub", self.common_desired_cap)
+        except Exception as e:
+            print(e)
+            if 'HTTPConnection' in str(e):
+                print('------------------------------------------------------------------')
+                print('You need run "appium --relaxed-security" from other terminal first !')
+                print('------------------------------------------------------------------')
+                exit(1)
+
+        self.driver.update_settings({
+            "waitForIdleTimeout": 3000,  # 3 seconds
+        })
+        print('done init')
+
+    def get_battery_level(self):
+        result = self.driver.battery_info
+
+        # out = result['stdout']
+        # print(out)
+        return result
+
+    def home(self, times=1):
+        for i in range(times):
+            self.driver.keyevent(3)
+            sleep(0.1)
+
+    def back(self, times=1):
+        for i in range(times):
+            self.driver.keyevent(4)
+            sleep(0.1)
+
+    def enter(self):
+        self.driver.keyevent(66)
+
+    def get_model(self):
+        m = self.driver.capabilities['deviceModel']
+        m = m.replace(' ', '')
+        return m
+
+    def save_screen(self, name):
+        """
+        Save screen with id, timestamp
+        :param name: full path name, ex. /Users/tt0622/screen_test
+        :return: /Users/tt0622/screen_test_xxx_2022-021359.png
+        """
+        f_name = '{}_{}_{}.png'.format(name, self.adb_id, time.strftime("%Y%m%d-%H%M%S"))
+        self.driver.save_screenshot(f_name)
+
+    def appium_touch_move_down(self):
+        try:
+            touch = TouchAction(self.driver)
+            touch.long_press(x=500, y=550).move_to(x=500, y=1300).release().perform()
+            sleep(3)
+        except Exception as e:
+            print(e)
+            print("Unable to scroll down")
+
+    def g7_login_later(self):
+        try:
+            self.driver.find_element_by_id('com.dexcom.g7:id/id_login_later_button').click()
+        except Exception as e:
+            print('Login screen failed ' + str(e))
+
+    def g7_re_login(self, user_nm, pass_wd):
+        try:
+            self.driver.find_element_by_id('com.dexcom.g7:id/id_login_now_button').click()
+            sleep(30)
+            self.driver.find_element_by_id('login_id_btn').click()
+            # username = self.driver.find_element_by_xpath("//android.widget.EditText[@resource-id='username']")
+            # username.click()
+            # username.send_keys(user_nm)
+            # # self.driver.keyevent(61)
+            # passwd = self.driver.find_element_by_xpath("//android.widget.EditText[@resource-id='password']")
+            # passwd.send_keys(pass_wd)
+            # self.driver.keyevent(66)
+            # sleep(60)
+            # self.driver.find_element_by_xpath("(//android.widget.CheckBox)[1]").click()
+            # self.driver.find_element_by_xpath("(//android.widget.CheckBox)[2]").click()
+        except Exception as e:
+            print('Login failed ' + str(e))
+
+    def g7_login(self, user_nm, pass_wd):
+        try:
+            self.driver.find_element_by_id('com.dexcom.g7:id/id_login_button').click()
+            sleep(30)
+            username = self.driver.find_element_by_xpath("//android.widget.EditText[@resource-id='username']")
+            username.click()
+            username.send_keys(user_nm)
+            # self.driver.keyevent(61)
+            passwd = self.driver.find_element_by_xpath("//android.widget.EditText[@resource-id='password']")
+            passwd.send_keys(pass_wd)
+            self.driver.keyevent(66)
+            sleep(60)
+            self.driver.find_element_by_xpath("(//android.widget.CheckBox)[1]").click()
+            self.driver.find_element_by_xpath("(//android.widget.CheckBox)[2]").click()
+        except Exception as e:
+            print('Login failed ' + str(e))
+
+        try:
+            username = self.driver.find_element_by_id('mat-input-0')
+            username.click()
+            username.send_keys(user_nm)
+        except Exception as e:
+            print('Different login failed ' + str(e))
+
+    def get_egv(self):
+        egv = ''
+        try:
+            egv = self.driver.find_element_by_id('com.dexcom.g7:id/id_glucose_compass_egv').text
+        except NoSuchElementException as e:
+            print('No EGV ' + str(e))
+        finally:
+            return egv
+
+    def verify_and_ack_alert(self):
+        ok = None
+        try:
+            ok = self.driver.find_element_by_id('com.dexcom.g7:id/id_alert_acknowledge_button')
+        except NoSuchElementException as e:
+            print('No Alert ok' + str(e))
+        finally:
+            if ok:
+                ok.click()
+                return True
+            else:
+                return False
+
+    def g7_verify_signal_loss_message(self):
+        """
+        Verify signal loss after 10 minutes with message
+        :return:
+        """
+        lost_help = lost_title = None
+        try:
+            lost_help = self.driver.find_element_by_id('com.dexcom.g7:id/id_glucose_state_card_help_button')
+        except NoSuchElementException as e:
+            print('Get lost_help button failed ' + str(e))
+
+        try:
+            lost_title = self.driver.find_element_by_id('com.dexcom.g7:id/id_glucose_state_card_title_label')
+        except NoSuchElementException as e:
+            print('Get lost_title  failed ' + str(e))
+
+        if lost_help and lost_title and lost_title.text == 'Signal Loss':
+            return True
+        else:
+            return False
+
+    def g7_click_ok_alert_ack(self):
+        try:
+            self.driver.find_element_by_id('com.dexcom.g7:id/id_alert_acknowledge_button').click()
+        except Exception as e:
+            print('Clike ack alert failed ' + str(e))
+
+        try:
+            self.driver.find_element_by_id('com.dexcom.g7:id/snackbar_action').click()
+        except Exception as e:
+            print('Click bar failed ' + str(e))
+
+    def g7_verify_signal_loss_alert(self):
+        """
+        Verify signal loss after 20 minutes with message
+        :return:
+        """
+        lost_ok = lost_title = None
+        try:
+            lost_ok = self.driver.find_element_by_id('com.dexcom.g7:id/id_alert_acknowledge_button')
+        except NoSuchElementException as e:
+            print('Get lost_ok button failed ' + str(e))
+
+        try:
+            lost_title = self.driver.find_element_by_id('com.dexcom.g7:id/id_alert_title_label')
+        except NoSuchElementException as e:
+            print('Get lost_title  failed ' + str(e))
+
+        if lost_ok and lost_title and lost_title.text == 'Signal Loss':
+            return True
+        else:
+            return False
+
+    def appium_tap_title_contains(self, text):
+        titles = self.driver.find_elements_by_id('android:id/title')
+        for t in titles:
+            if text in t.text:
+                t.click()
+                sleep(2)
+                break
+
+    def appium_touch_move_up(self):
+        try:
+            touch = TouchAction(self.driver)
+            touch.long_press(x=500, y=1300).move_to(x=500, y=550).release().perform()
+            sleep(3)
+        except Exception as e:
+            print(e)
+            print("Unable to scroll up")
