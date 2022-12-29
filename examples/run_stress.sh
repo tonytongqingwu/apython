49ab99589144d78abbf01c5b28c092e2e55944c8
tonytongqingwu <tony.wu@dexcom.com>
2022-12-29 09:53:55 -0800

    add python path

--- examples/run_stress.sh
+++ examples/run_stress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 osascript  <<EOF
 tell app "Terminal"
-  do script "cd ~/Sandbox/apython/examples && python3 stress.py &"
+  do script "cd ~/Sandbox/apython/examples && /opt/homebrew/bin/python3.10 stress.py &"
 end tell
 EOF
